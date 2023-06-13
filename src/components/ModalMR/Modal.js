da9c940ee51026ae912fd11c87dfb2724f32ce9c
Jorge Silva <jorgelfsilva5@gmail.com>
2023-06-13 12:44:26 -0300

    fix - alteração no como de cadastrar uma solicitação

--- src/components/ModalMR/Modal.js
+++ src/components/ModalMR/Modal.js
@@ -4,42 +4,108 @@ import { SolictContext } from '../../context/mrSolictContext'
 import api from '../../services/api'
 
 const Modal = ({data}) => {
-  const {
-    nome, 
-    QTDP,
-    unidade,
-    error,
-    handleNome,
-    handleQTDP,
-    handleUnidade,
-    handleSubmit,
-    produtos,
-
-  } = React.useContext(SolictContext)
-
-  const [filterProdutos, setFilterProdutos] = React.useState([])
+  const [nome, setNome] = React.useState('')
+  const [QTDP, setQTDP] = React.useState('')
+  const [unidade, setUnidade] = React.useState('')
+  const [url, setUrl] = React.useState('')
+  const [error, setError] = React.useState(false)
+  const [user, setUser] = React.useState(false)
   const [produto, setProduto] = React.useState(false)
+  const [filterProdutos, setFilterProdutos] = React.useState([])
 
- /*  async function getProdutos(){
+  async function getProdutos(){
     await api.get(`/secretaria/produtos/${data.secretaria}`)
         .then(({data}) => setProduto(data))
           .catch(e => console.log(e))
   }
-  
-  console.log(produto);
 
-  const handleFilter = () => {
-    const search = nome
-    const newFilter = produto.filter((item) => {
-      return item.nome.includes(search)
-    })
+  const handleNome = (e) => {
+    setNome(e.target.value)
+    const search = e.target.value
+    if(produto){
+      const newFilter = produto.filter((value) => {
+        return value.nome.toLowerCase().includes(search.toLowerCase())
+        setUnidade(value.unidadeMedida)
+      })
 
-    setFilterProdutos(newFilter)
-  } */
+      if(search == ""){
+        setFilterProdutos([])
+      }else{
+        setFilterProdutos(newFilter)
+      }
+    }
+  }
+
+  const handleQTDP = (e) => {
+    setQTDP(e.target.value)
+  }
+
+  const handleUnidade = (e) => {
+    setUnidade(e.target.value)
+  }
+
+
+  const handleUrl = (e) => {
+    setUrl(String(document.URL.split("/").slice(-1)))
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    save()
+  }
+
+  console.log(nome);
+
+  async function save() {
+    if(!nome){
+      return alert('Produto precisa ser nomeado.')
+    }else if(!QTDP){
+      return alert('Precisa ser informado a quantidade.')
+    }else if(!unidade){
+      return alert('Precisa ser informado a unidade de medida.')
+    }
+
+    const {data} = await api.get(`/usuario/${url}`)
+
+    try{
+      setUser(data)
+
+      if(data){
+        await api.get(`/secretaria/produtos/${data.secretaria}`)
+          .then(({data}) => {
+            if(data){
+              for(let i = 0; i < data.length; i++){
+                if(data && nome == data[i].nome){
+                  let hora = new Date()
+
+                  api.post('/mr', {
+                    nome: nome,
+                    quantidadeProduto: QTDP,
+                    unidadeMedida: unidade,
+                    merendeira: url,
+                    produto: data[i]._id,
+                    secretaria: data[i].secretaria,
+                    horario:  `${String(hora.getDate())}/${String(hora.getMonth()+1)}/${hora.getFullYear()}T${hora.getHours()}:${String(hora.getMinutes())}`
+                  }).then(() => {
+                    alert('Solicitação enviada com sucesso.')
+                    window.location.reload()
+                  })
+                  .catch(error => setError(error.response.data.error))
+                }
+              }
+            }
+          }
+        )
+      }
+    }catch(e){
+      setError(e.response.data.error)
+    }
+  }
 
   React.useEffect(() => {
+    handleUrl()
     if(data){
-      // getProdutos()
+      getProdutos()
     }
   }, [])
   
@@ -56,19 +122,19 @@ const Modal = ({data}) => {
                 onChange={handleNome}
                 id={nome}
                 required
+                className={style.input_nome}
               />
 
               <label htmlFor={nome}>Nome Produto</label>
               {
-                filterProdutos.lenght &&
+                filterProdutos.length != 0 &&
                 <div className={style.card__modal__list__content}>
                   {
-                    produtos &&
-                    filterProdutos.map((item) => {
-                      <p className={style.card__modal__list}>
-                        {item.nome}
+                    filterProdutos.map(item => 
+                      <p key={item._id}>
+                        {`${item.nome} ${item.quantidadeProduto} ${item.unidadeMedida} no almoxarifado`}
                       </p>
-                    })
+                   )
                   }
                 </div>
               }
